ab7fc6e7cba2439ddaa8171c0c6c228467e6126c
Aquamoris <pestryak.nikita.dev@gmail.com>
2023-07-27 23:19:41 +1000

    add component FilmDetails

---
+++ src/components/FilmDetails.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface Props {
+    title: string;
+    genre: 'comedy' | 'horror';
+    seasonsCount: number;
+}
+
+export const FilmDetails: FunctionComponent<Props> = ({}) => {
+    return null;
+};
